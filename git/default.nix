7c8e904120431b3b73185ce1d56142693dec53a3
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-09-26 19:33:07 -0500

    Add aliases within git

--- git/default.nix
+++ git/default.nix
@@ -8,6 +8,14 @@
     enable = true;
     userName = "Collin Williams";
     userEmail = "96917990+bluedragon1221@users.noreply.github.com";
-    extraConfig = { init = { defaultBranch = "main"; }; };
+    extraConfig = {
+      init = {
+	defaultBranch = "main";
+      };
+    };
+    aliases = {
+      sb = "status -sb";
+      unstage = "restore --staged";
+    };
   };
 }
