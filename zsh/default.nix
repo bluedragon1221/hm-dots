57da8e2485d077a96b7ae397ff0433801cab84f3
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-09-26 19:37:02 -0500

    Switch to [starship prompt](https://starship.rs)

--- zsh/default.nix
+++ zsh/default.nix
@@ -1,6 +1,6 @@
 # vim: sw=2
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   home.packages = with pkgs; [
@@ -32,16 +32,67 @@
     };
 
     dirHashes = {
-      doc = "$HOME/Documents";
-      vid = "$HOME/Videos";
-      dl = "$HOME/Downloads";
-      cfg = "$HOME/.config";
-      p = "$HOME/projects";
-      hm = "$HOME/.config/home-manager";
-      py = "$HOME/projects/learning_python";
-      nvim = "$HOME/.config/nvim";
-      r = "$HOME/projects/ROBOTICS";
+      doc = "~/Documents";
+      vid = "~/Videos";
+      dl = "~/Downloads";
+
+      cfg = "~/.config";
+      hm = "~cfg/home-manager";
+      nvim = "~cfg/nvim";
+
+      p = "~/projects";
+      py = "~p/learning_python";
+      r = "~p/ROBOTICS";
     };
     initExtra = (builtins.readFile ./initExtra.zsh);
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = false;
+      format = lib.concatStrings [
+	"$directory$git_branch"
+	" $git_status"
+	"$character"
+      ];
+      scan_timeout = 10;
+      character = {
+	success_symbol = "[λ](green bold)";
+	error_symbol = "[λ](red bold)";
+      };
+      git_branch.format = "[@$branch](underline)";
+      git_status = {
+	format = "[*$conflicted$untracked$modified$staged$renamed$deleted](218) ";
+	style = "cyan";
+	conflicted = "";
+	untracked = "?";
+	modified = "M";
+	staged = "S";
+	renamed = "";
+	deleted = "D";
+	stashed = "≡";
+      };
+      directory = {
+	format = "[$path](underline)";
+	truncation_length = 5;
+	truncate_to_repo = false;
+	home_symbol = "~";
+	substitutions = {
+	  "~/Documents" = "~doc";
+	  "~/Videos" = "~vid";
+	  "~/Downloads" = "~dl";
+
+	  "~/.config" = "~cfg";
+	  "~cfg/home-manager" = "~hm";
+	  "~cfg/nvim" = "~nvim";
+
+	  "~/projects" = "~p";
+	  "~p/learning_python" = "~py";
+	  "~p/ROBOTICS" = "~r";
+	};
+      };
+    };
   };
 }
--- zsh/initExtra.zsh
+++ zsh/initExtra.zsh
@@ -2,39 +2,6 @@
 printf '\n%.0s' {1..100}
 alias clear="clear; printf '\n%.0s' {1..100}"
 
-# Execution Time
-# Source: https://gist.github.com/knadh/123bca5cfdae8645db750bfb49cb44b0
-function preexec() {
-    timer=$(($(date +%s%0N)/1000000))
-}
-
-function precmd() {
-    if [ $timer ]; then
-	now=$(($(date +%s%0N)/1000000))
-	elapsed=$(($now-$timer))
-
-	RPROMPT="%F{cyan}${elapsed}ms%f"
-	unset timer
-    fi
-}
-
-autoload -Uz add-zsh-hook vcs_info
-setopt prompt_subst
-add-zsh-hook precmd vcs_info
-
-PROMPT='%F{red}${vcs_info_msg_0_}%f '    # Git Status
-PROMPT+='%U%~%u '                        # Current Working Directory
-PROMPT+='%B%(?.%F{green}.%F{red})λ%f%b ' # Prompt Character
-
-# Enable checking for (un)staged changes, enabling use of %u and %c
-zstyle ':vcs_info:*' check-for-changes true
-# Set custom strings for an unstaged vcs repo changes (*) and staged changes (+)
-zstyle ':vcs_info:*' unstagedstr ' *'
-zstyle ':vcs_info:*' stagedstr ' +'
-# Set the format of the Git information for vcs_info
-zstyle ':vcs_info:git:*' formats       '(%b%u%c)'
-zstyle ':vcs_info:git:*' actionformats '(%b|%a%u%c)'
-
 source ~/.nix-profile/etc/profile.d/nix.sh
 
 PATH+=":$HOME/.local/bin"
