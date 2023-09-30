97988d0d8fe2dbb85c3e4fcb6459a095525390b2
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-09-30 14:42:22 -0500

    Finally figured out flakes!

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Home Manager configuration of Collin Williams";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.collin = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+          {
+            home = {
+              username = "collin";
+              homeDirectory = "/home/collin";
+              stateVersion = "23.05";
+            };
+          }
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
