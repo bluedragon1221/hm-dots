380f22d75870a02ab52b151228efd4ee6e530452
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-10-01 16:19:22 -0500

    I can have alejandra, deadnix, and statix as formatters!

--- flake.nix
+++ flake.nix
@@ -10,31 +10,40 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    alejandra = {
-      url = "github:kamadorueda/alejandra/3.0.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-formatter-pack.url = "github:Gerschtli/nix-formatter-pack";
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    alejandra,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    homeConfigurations.collin = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules = [./home.nix];
+  outputs =
+    { nixpkgs
+    , home-manager
+    , nix-formatter-pack
+    , ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations.collin = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+
+      formatter.${system} = nix-formatter-pack.lib.mkFormatter {
+        inherit nixpkgs;
+        system = "x86_64-linux";
+
+        # extensible with custom modules:
+        # extraModules = [ otherFlake.nixFormatterPackModules.default ];
+
+        config.tools = {
+          deadnix.enable = true;
+          nixpkgs-fmt.enable = true;
+          statix.enable = true;
+        };
+      };
+
+      # "nix run ." will run home-manager switch
+      defaultPackage.${system} = home-manager.defaultPackage.${system};
     };
-
-    formatter.${system} = [
-      alejandra.defaultPackage.${system}
-    ];
-
-    # "nix run ." will run home-manager switch
-    defaultPackage.${system} = home-manager.defaultPackage.${system};
-  };
 }
