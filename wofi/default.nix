fc6b6995065ef48225ff26f03d091efefcbc1331
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-10-01 08:45:12 -0500

    wofi launcher

---
+++ wofi/default.nix
@@ -0,0 +1,16 @@
+# vim: sw=2
+
+{ ... }:
+
+{
+  programs.wofi = {
+    enable = true;
+    settings = {
+      width = 650;
+      height = 256;
+      mode = "drun";
+      filter_rate = 100;
+    };
+    style = (builtins.readFile ./style.css);
+  };
+}
