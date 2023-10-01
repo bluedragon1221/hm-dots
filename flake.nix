ad973c8bd58d60f21e0f1b1126fb7bba5655f64d
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-10-01 08:43:56 -0500

    move home stuff to home.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
+# vim: sw=2
+
 {
   description = "Home Manager configuration of Collin Williams";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -20,19 +22,13 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [
-          ./home.nix
-          {
-            home = {
-              username = "collin";
-              homeDirectory = "/home/collin";
-              stateVersion = "23.05";
-            };
-          }
-        ];
+        modules = [ ./home.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
+        extraSpecialArgs = { inherit inputs; };
       };
+
+      formatter.${system} = "nixfmt";
     };
 }
