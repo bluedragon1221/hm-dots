dd4abcde4b92301956726fd0127daa598064e8d7
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2023-10-18 15:24:38 -0500

    remove neovim configuration

--- neovim/default.nix
+++
@@ -1,8 +0,0 @@
-# vim: sw=2
-_: {
-  programs.neovim.enable = true; # installs neovim
-  home.file.".config/nvim" = {
-    source = ./files;
-    recursive = true;
-  };
-}
--- neovim/files/README.md
+++
@@ -1,2 +0,0 @@
-# nvim-config
-My Neovim config. Featuring lazy.nvim, lsp, treesitter, and more!
